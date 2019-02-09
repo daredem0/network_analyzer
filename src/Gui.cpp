74ab150e0b90fce2bfde1364a058090337b9a00a
Florian Leuze <florianleuze@aol.com>
2019-02-09 22:59:39 +0100

    worked on signal handlers_3

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -25,29 +25,29 @@ Gui::Gui(const Gui& orig) {
 Gui::~Gui() {
 }
 
- GObject *Gui::gui_get_ui_element(const gchar * name)
-    {
-     
-        std::cout << "you ended up here a" << std::endl;
-        const gchar *s;
-        GSList *list;
-        int temp = 0;
-        
-        list = this->objects;
-        do{
-            std::cout << "you ended up here c: " << std::to_string(temp) << std::endl;
-            std::cout << gtk_buildable_get_name((GtkBuildable *)list->data) << std::endl;
-            s = gtk_buildable_get_name((GtkBuildable *)list->data);
-            std::cout << "you ended up here c: " << std::to_string(temp) << std::endl;
-            ++temp;
-            if (strcmp (s, name) == 0) {
-                return (GObject *)list->data;
-            }
-    
-        } while (list = g_slist_next(list));
-        std::cout << "you ended up here c" << std::endl;
-        return NULL;
-    }   
+GObject *Gui::gui_get_ui_element(const gchar * name)
+{
+
+    std::cout << "you ended up here a" << std::endl;
+    const gchar *s;
+    GSList *list;
+    int temp = 0;
+
+    list = this->objects;
+    do{
+        std::cout << "you ended up here c: " << std::to_string(temp) << std::endl;
+        std::cout << gtk_buildable_get_name((GtkBuildable *)list->data) << std::endl;
+        s = gtk_buildable_get_name((GtkBuildable *)list->data);
+        std::cout << "you ended up here c: " << std::to_string(temp) << std::endl;
+        ++temp;
+        if (strcmp (s, name) == 0) {
+            return (GObject *)list->data;
+        }
+
+    } while (list = g_slist_next(list));
+    std::cout << "you ended up here c" << std::endl;
+    return NULL;
+}   
     
 void gui_init(Gui *gui)
 {
